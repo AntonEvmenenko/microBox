8b9e09141bc29b49dfd5c3e94f1bcfaefbb61183
Anton Evmenenko <anton.evmenenko.93@gmail.com>
2020-07-29 12:43:13 +0200

    unused code removed

--- microBox.h
+++ microBox.h
@@ -26,23 +26,6 @@
 #define ESC_STATE_START 1
 #define ESC_STATE_CODE 2
 
-// #define TELNET_IAC 255
-// #define TELNET_WILL 251
-// #define TELNET_WONT 252
-// #define TELNET_DO 253
-// #define TELNET_DONT 254
-
-// #define TELNET_OPTION_ECHO 1
-// #define TELNET_OPTION_SGA 3
-
-// #define TELNET_STATE_NORMAL 0
-// #define TELNET_STATE_IAC 1
-// #define TELNET_STATE_WILL 2
-// #define TELNET_STATE_WONT 3
-// #define TELNET_STATE_DO 4
-// #define TELNET_STATE_DONT 5
-// #define TELNET_STATE_CLOSE 6
-
 typedef struct
 {
     const char *cmdName;
@@ -71,34 +54,14 @@ public:
     bool AddCommand(const char *cmdName, void (*cmdFunc)(char **param, uint8_t parCnt));
 
 private:
-    // static void ListDirCB(char **pParam, uint8_t parCnt);
-    // static void ListLongCB(char **pParam, uint8_t parCnt);
-    // static void ChangeDirCB(char **pParam, uint8_t parCnt);
-    // static void EchoCB(char **pParam, uint8_t parCnt);
-    // static void CatCB(char** pParam, uint8_t parCnt);
     static void watchCB(char** pParam, uint8_t parCnt);
     static void watchcsvCB(char** pParam, uint8_t parCnt);
-    // static void LoadParCB(char **pParam, uint8_t parCnt);
-    // static void SaveParCB(char **pParam, uint8_t parCnt);
-
-    // void ListDir(char **pParam, uint8_t parCnt, bool listLong=false);
-    // void ChangeDir(char **pParam, uint8_t parCnt);
-    // void Echo(char **pParam, uint8_t parCnt);
-    // void Cat(char** pParam, uint8_t parCnt);
-    // void watch(char** pParam, uint8_t parCnt);
-    // void watchcsv(char** pParam, uint8_t parCnt);
-
+    
 private:
     void ShowPrompt();
     uint8_t ParseCmdParams(char *pParam);
     void ErrorDir(const __FlashStringHelper *cmd);
-    // char *GetDir(char *pParam, bool useFile);
-    // char *GetFile(char *pParam);
-    // void PrintParam(uint8_t idx);
-    // int8_t GetParamIdx(char* pParam, bool partStr = false, int8_t startIdx = 0);
     int8_t GetCmdIdx(char* pCmd, int8_t startIdx = 0);
-    // uint8_t Cat_int(char* pParam);
-    // void ListDirHlp(bool dir, bool rw = true, int len=4096);
     uint8_t ParCmp(uint8_t idx1, uint8_t idx2, bool cmd=false);
     void HandleTab();
     void HistoryUp();
@@ -106,12 +69,9 @@ private:
     void HistoryPrintHlpr();
     void AddToHistory(char *buf);
     void ExecCommand();
-    // void handleTelnet(uint8_t ch);
-    // void sendTelnetOpt(uint8_t option, uint8_t value);
     double parseFloat(char *pBuf);
     bool HandleEscSeq(unsigned char ch);
-    // void ReadWriteParamEE(bool write);
-
+    
 private:
     char currentDir[MAX_PATH_LEN];
 
@@ -119,7 +79,6 @@ private:
     char dirBuf[15];
     char *ParmPtr[10];
     uint8_t bufPos;
-    // bool watchMode;
     bool csvMode;
     uint8_t escSeq;
     unsigned long watchTimeout;
@@ -129,11 +88,9 @@ private:
     int historyWrPos;
     int historyCursorPos;
     bool locEcho;
-    // uint8_t stateTelnet;
 
     static CMD_ENTRY Cmds[MAX_CMD_NUM];
     PARAM_ENTRY *Params;
-    static const char dirList[][5] PROGMEM;
 };
 
 extern microBox microbox;
